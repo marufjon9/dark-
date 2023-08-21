330d7953026a66d16b64bc63128a8912f8c0b8a5
marufjon9 <mabduraupovv@gmail.com>
2023-08-21 21:09:40 +0500

    Initial Commit

---
+++ countries.js
@@ -0,0 +1,58 @@
+const countries = [
+  {
+    name: "Germany",
+    population: "81,770,900",
+    region: "Europe",
+    capital: "Berlin",
+    img: "Germany.png",
+  },
+  {
+    name: "United States of America",
+    population: "323,947,000",
+    region: "Americas",
+    capital: "Washington, D.C.",
+    img: "USA.png",
+  },
+  {
+    name: "Brasil",
+    population: "206,135,893",
+    region: "Americas",
+    capital: "Brasilia",
+    img: "Brasil.png",
+  },
+  {
+    name: "Iceland",
+    population: "334,300",
+    region: "Europe",
+    capital: "Reykjavík",
+    img: "Iceland.png",
+  },
+  {
+    name: "Afghanistan",
+    population: "27,657,145",
+    region: "Asia",
+    capital: "Kabul",
+    img: "Afghanistan.png",
+  },
+  {
+    name: "Aland Islands",
+    population: "28,875",
+    region: "Europe",
+    capital: "Mariehamn",
+    img: "AlanIslands.png",
+  },
+  {
+    name: "Albania",
+    population: "2,886,026",
+    region: "Europe",
+    capital: "Tirania",
+    img: "Albania.png",
+  },
+  {
+    name: "Algeria",
+    population: "40,400,000",
+    region: "Africa",
+    capital: "Algiers",
+    img: "Algeria.png",
+  },
+];
---
+++ script.js
@@ -0,0 +1,66 @@
+const themeBtn = document.querySelector(".header__theme");
+const list = document.querySelector(".main__list");
+
+// themeBtn.addEventListener("click", () => {
+//   document.body.classList.toggle("dark");
+// });
+
+const renderCountries = (array) => {
+  array.forEach((element) => {
+    const li = document.createElement("li");
+    li.classList.add("main__item");
+
+    const img = document.createElement("img");
+    img.setAttribute("src", `/img/${element.img}`);
+    img.classList.add("item__img");
+
+    const h3 = document.createElement("h3");
+    h3.classList.add("item__title");
+    h3.textContent = element.name;
+
+    const div = document.createElement("div");
+    div.classList.add("item__textbox");
+
+    const population = document.createElement("p");
+    population.classList.add("item__population");
+    population.innerHTML = `<strong>Population:</strong> ${element.population}`;
+
+    const region = document.createElement("p");
+    region.classList.add("item__region");
+    region.innerHTML = `<strong>Region:</strong> ${element.region}`;
+
+    const capital = document.createElement("p");
+    capital.classList = document.createElement("p");
+    capital.innerHTML = `<strong>Capital:</strong> ${element.capital}`;
+
+    div.append(population, region, capital);
+    li.append(img, h3, div);
+    list.append(li);
+  });
+};
+
+renderCountries(countries);
+
+if (localStorage.getItem("mode") == "dark") {
+  darkMode();
+} else {
+  noDark();
+}
+
+themeBtn.addEventListener("click", function () {
+  if (document.body.classList.toggle("dark")) {
+    darkMode();
+  } else {
+    noDark();
+  }
+});
+
+function darkMode() {
+  document.body.classList.add("dark");
+  localStorage.setItem("mode", "dark");
+}
+
+function noDark() {
+  document.body.classList.remove("dark");
+  localStorage.setItem("mode", "light");
+}
