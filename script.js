55521e4597482d76b4dcacd1c280be4be3a2209c
marufjon9 <mabduraupovv@gmail.com>
2023-08-22 14:59:19 +0500

    Error fix

--- script.js
+++ script.js
@@ -49,6 +49,7 @@ if (localStorage.getItem("mode") == "dark") {
 
 themeBtn.addEventListener("click", function () {
   document.body.classList.toggle("dark");
+
   if (document.body.classList.contains("dark")) {
     darkMode();
   } else {
@@ -59,9 +60,11 @@ themeBtn.addEventListener("click", function () {
 function darkMode() {
   document.body.classList.add("dark");
   localStorage.setItem("mode", "dark");
+  themeBtn.textContent = "Light Mode";
 }
 
 function noDark() {
   document.body.classList.remove("dark");
   localStorage.setItem("mode", "light");
+  themeBtn.textContent = "Dark Mode";
 }
